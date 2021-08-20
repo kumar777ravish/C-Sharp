cd5ddf176e71829ca866c98bf158cc68cb57b1ec
RAVISH KUMAR <89187239+kumar777ravish@users.noreply.github.com>
2021-08-20 20:43:24 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,174 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace OOP_s1
+namespace OOP_s1ProgramHousingLoanCustomer
 {
-    class Student
-    {
-        int Sid;
-        string Name;
-        double M1, M2;
 
-        public void CreateStudent(int id, string name)
-
-        public void SetMarks(double m1, double m2)
-
-        public void Results()
-    }
     class Program
     {
-        Student S;
-        S = new Student();
+        //class HousingLoanCustomer
+        //{
+        //    int Customer Id; 
+        //    string Name;
+        //    double LoanAmount;
+        //    static double _ROI;
 
-        Console.WriteLine("Enter Sid And Name");
-            int id = int.Parse(Console.ReadLine());
-        string name = Console.ReadLine();
+        //    public static double ROI
+        //    {
+        //        set { return _ROI; }
+        //        set { _ROI = value; }
+        //    }
+        //    static HousingLoanCustomer()
+        //    {
+        //        ROI = 11.2;
+        //    }
 
-        S.CreateStudent(id, name);
+        //    public HousingLoanCustomer(int Customer Id, string Name, double LoanAmount, double ROI)
+        //    {
+        //        this.CustomerId = CustomerId;
+        //        this.Name = Name;
+        //        this.LoanAmount = LonAmount;
+        //    }
 
-            Console.WriteLine("Enter Marks 1 and 2");
-            double m1 = double.Parse(Console.ReadLine());
-        double m2 = double.Parse(Console.ReadLine());
+        //    public void CalculateIntrest(int months)
+        //    {
+        //        double IntAmount = LoanAmount = ROI / 100;
+        //        double RepayAmount = LoanAmount + IntAmount;
+        //        Console.WriteLine("LoanAmount:(0) IntAmount:(1) RePayAmount:(2) ROI:(3)", LoanAmount, IntAmount, RepayAmount, _ROI);
+        //        Console.WriteLine("EMI:" + (RepayAmount / months));
+        //    }
 
-        S.SetMarks(m1, m2);
+        //    public static void Enquiry(double EnLoanAmount, int months)
+        //    {
+        //        Console.WriteLine("ROI :" + ROI);
+        //        Console.WriteLine("LoanAmount:" + EnLoanAmount);
+        //        Console.WriteLine("IntAmount" + EnLoanAmount + ROI / 100);
+        //        Console.Writeline("RepayAmount" + (EnLoanAmount + (EnLoanAmount + ROI / 100)));
+        //        Console.WriteLine("EMI' Moths:" + months);
+        //        Console.WriteLine("EMI's:" + (EnLoanAmount + (EnLoanAmount + ROI / 100)) / months);
+        //    }
+        //}
 
-            S.Result();
-            S.CreateStudent(200, "Ravish"); //Re-initialise the object - issue 2
+        //static void Main(string[] args)
+        //{
 
+        //    var loanCustomer1 = new HousingLoanCustomer(1, "Peter", 75000);
+        //    loanCustomer1.CalculateIntrest(12);
 
-            //Student S1 = new Student();
-            //S1.CreateStudent(200, "Ravish");
-            Console.WriteLine("Hello World!");
-        }
+        //    var LoanCustomer2 = new HousingLoanCustomer(2, "Jack", 85000,);
+        //    loanCustomer2.CalculateIntrest(24);
 
-    class,Fields,Methods And Objects
+        //    HousingLoanCustomer.ROI = 11.5;
 
-        class Student
-    {
-        int Sid;
-        string Name;
-        double M1, M2;
-    }
-    public Student(int id, string name)
-    {
-        Sid = id;
-        name = name;
-    }
-    public void SetMarks(double m1, double m2)
-    {
-        M1 = m1;
-        M2 = m2;
-    }
-    public void Result()
-    {
-        duble avg = (M1 + M2) / 2;
-        if (avg >= 35)
-        {
-            Console.WriteLine("Pass");
-        }
-        else
-        {
+        //    loanCustomer1.CalculateIntrest(12);
+        //    loanCustomer2.CalculateIntrest(24);
 
+        //    HousingLoanCustomer.Enquiry(60000, 24);
 
-            Console.WriteLine("Fail");
-        }
-    }
-}
-class Program
-{
-    static void Main(string[] args)
-    {
-        Student S = new Student(100, "peter");
-        S.SetMarks(23, 45);
+        //var LoanCustomer3 = new HousingLoanCustomer(3), "sanjay", 175000,);
+        //var LoanCustomer3 = new HousingLoanCustomer(4), "karthik", 80000,);
 
-        S.Result();
-        //S.Student(200, "Ravish");
-
-        #region Constructors
-        class Student
-    {
-        int Sid;
-        string Name;
-        double M1, M2;
-        string Contact;
-
-        public Student()
-        {
-            Console.WriteLine("This is default Constructors")
-        }
-
-        public S(int id, string name, string contact)
-        {
-            Sid = id;
-            Name = name;
-            Contact = contact;
-        }
-
-        public void SetMarks(m1, m2)
-        {
-            M1 = m1;
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Student S1 = new Student(200, "Ravish");
+        static-Fields,Constructors,Properties & methods
 
 
-            Student S = new Student(100, "Peter");
-
-            S.SetMarks(23, 45);
-
-            S.Result();
-
-            //S.Student(200, "Ravish");
-
-            public void Result()
-            {
-                double avg = (M1 + M2) / 2;
-                if (avg >= 35)
-                {
-                    Console.WriteLine("Pass");
-                }
-                else
-                {
-                    Console.WriteLine("Fail");
-                }
-            }
-            public void Result(double minAvgToPass)
-            {
-                double avg = (M1 + M2) / 2;
-                if (avg >= minAvgToPass)
-                {
-                    Console.WriteLine("Pass");
-                }
-                else
-                {
-                    Console.WriteLine("Fail");
-                }
-            }
-        }
-
-        Constructors
-
-        Constructors Overloading, this keyboard, Method Overloading
-
-
-        int Sid;
-        string Name;
-        public student(int id, string Name)
-        {
-            this.Sid = Sid;
-            this.Name;
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        { 
-            var s = new Student(100, "peter")
-            int Sid =GetSid
-            Console
 
 
 
@@ -176,63 +81,5 @@ class Program
 
 
     }
-
-
-  
-        {  
-                var S = new Student
-
-
-
-
-
-
-
-
-
-
-
-
-        Console.ReadLine();
-        }
-    }
-
-
-
-
-
-
-
-          
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-    
-    
-        
-        
-    
-
-        
-       
-    
-
-
-    
-    {
-       
-        {
-        }
     }
 }
